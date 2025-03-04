c96bc097a606f6929b6574000f2e5fb3392f5d4c
darkninjas86 <zishan.malik@curiositycottage.com>
2025-03-04 13:15:20 -0800

    updated models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from typing import List, Optional
-from datetime import date
+from datetime import date, datetime
 
 # ✅ Define EndCustomer Model
 class EndCustomer(BaseModel):
